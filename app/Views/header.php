618db07b94493cb101032d2f88edebbf67e5aa7d
Mooch <t.tonneau@gmail.com>
2017-09-09 17:19:09 +0200

    fix js bootstrap

--- app/Views/header.php
+++ app/Views/header.php
@@ -6,18 +6,30 @@
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="index.php?action=login">WebSiteName</a>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php?action=login">Login</a></li>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php?action=login">Login</a></li>
       <li><a href="index.php?action=inventaire">Inventaire</a></li>
       <li><a href="index.php?action=liste-lot">Liste des lots</a></li>
       <li><a href="index.php?action=recette">Recette</a></li>
       <li><a href="index.php?action=logout">Déconnexion</a></li>
     </ul>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
   </div>
 </nav>
 	
