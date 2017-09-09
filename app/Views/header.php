5fb0a995e5d9d7a429c5188b8071dca462641ddc
ModjoInc <habibem@gmail.com>
2017-09-09 15:22:54 +0200

    ini

--- app/Views/header.php
+++ app/Views/header.php
@@ -2,7 +2,9 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/reset.css">
+  <link rel="stylesheet" href="/css/master.css">
 	<title>Document</title>
 </head>
 <body>
-	
--- app/index.php
+++ app/index.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 define('VIEWS', 'Views/')
 
 
-require_once('header.php');
+require_once(VIEWS . 'header.php');
 
-require_once('footer.php');
+require_once(VIEWS.'footer.php');
 
 ?>
