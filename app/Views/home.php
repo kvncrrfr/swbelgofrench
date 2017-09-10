9bea427e00543471f5d65c7169f1ec710949aa2e
ModjoInc <habibem@gmail.com>
2017-09-10 15:27:23 +0200

    tweak home

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,12 +6,11 @@
   </div>
 
   <div class="homeSlogan">
-    <h3>Every <span class="homeFood">food</span> is a fresh start <span class="homeFood">
+    <h3>Every <span class="homeFood">food</span> is a fresh start<span class="homeFood">
       .</span></h3>
   </div>
 
 
-<p>helllo</p>
   <div class="homeNav">
     <h2><a href="index.php?action=login"><img src="<?= VIEW . 'img/home.png' ?>" alt="Are You Ready"></a></h2>
   </div>
