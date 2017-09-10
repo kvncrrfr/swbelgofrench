3f0ff0c0d34416e464ce6c973183972a0e27b60d
Mooch <t.tonneau@gmail.com>
2017-09-10 16:25:19 +0200

    viewport et liste lot

--- app/Views/header.php
+++ app/Views/header.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="/css/reset.css">
   <link rel="stylesheet" href="/css/master.css">
--- app/Views/js/script.js
+++ app/Views/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	if($('#inventaire').length != 0){
+		$('footer').hide();
+	}
+
 	$('.quantite').focusout(function () {
 		var input = $(this).val();
 		var isNumber = !isNaN(parseInt(input)); // teste si il s'agit d'un nombre (boolean)
@@ -13,4 +17,23 @@ $(document).ready(function () {
 	$('.article-inventaire').click(function () {
 		// focus dans l'input
 	});
+
+	if($('.frame').length != 0){
+		$('footer').hide();
+
+		$('.list-group').click(function () {
+			$('.frame .lot').css("border-color", "white");
+			var elem = $(this).find($('.lot-badge>i'));
+			$('.lot-badge>i.icofont-check-circled').removeClass("icofont-check-circled");
+			$('.lot-badge>i').addClass("icofont-close-circled");
+			if(elem.attr("class") == 'icofont icofont-close-circled'){
+				elem.removeClass("icofont-close-circled");
+				elem.addClass("icofont-check-circled");
+			}
+			$('.list-group>.lot').css('border-color', 'white');
+			$(this).children().first().css('border-color', 'green');
+		});
+	}
+
+	
 });
